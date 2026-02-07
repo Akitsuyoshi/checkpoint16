218c8e2a301420b1b894babca07c69bf712774a2
Tsuyoshi Akiyama <ka090824@gmail.com>
2026-02-07 05:07:02 +0000

    Add conversion from wheel_speed to cmd_vel topic

--- kinematic_model/src/kinematic_model.cpp
+++ kinematic_model/src/kinematic_model.cpp
@@ -17,15 +17,36 @@ public:
         "/wheel_speed", 10,
         [this](Float32MultiArray::SharedPtr msg) { callback(msg); });
     pub_ = create_publisher<Twist>("/cmd_vel", 10);
+
+    h_wheel_base_ = 0.170 / 2.0;
+    h_track_width_ = 0.26969 / 2.0;
+    radius_ = 0.100 / 2.0;
     RCLCPP_INFO(get_logger(), "Node initialized");
   }
 
 private:
   void callback(const Float32MultiArray::SharedPtr msg) {
-    RCLCPP_INFO(get_logger(), "msg is received");
+    auto u_1 = msg->data[0];
+    auto u_2 = msg->data[1];
+    auto u_3 = msg->data[2];
+    auto u_4 = msg->data[3];
+
+    float vx = radius_ / 4.0 * (u_1 + u_2 + u_3 + u_4);
+    float vy = radius_ / 4.0 * (-u_1 + u_2 - u_3 + u_4);
+    float vw = radius_ / (4.0 * (h_wheel_base_ + h_track_width_)) *
+               (-u_1 + u_2 + u_3 - u_4);
+
+    Twist cmd_vel;
+    cmd_vel.linear.x = vx;
+    cmd_vel.linear.y = vy;
+    cmd_vel.angular.z = vw;
+    pub_->publish(cmd_vel);
   }
   rclcpp::Subscription<Float32MultiArray>::SharedPtr sub_;
   rclcpp::Publisher<Twist>::SharedPtr pub_;
+  float h_wheel_base_;
+  float h_track_width_;
+  float radius_;
 };
 
 int main(int argc, char *argv[]) {
