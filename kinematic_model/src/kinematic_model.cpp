8d745a0686623ab71557c994eff2e68bd44173ed
Tsuyoshi Akiyama <ka090824@gmail.com>
2026-02-11 13:00:46 +0000

    Refactor kinematic model

--- kinematic_model/src/kinematic_model.cpp
+++ kinematic_model/src/kinematic_model.cpp
@@ -4,9 +4,32 @@
 #include <vector>
 
 #include "geometry_msgs/msg/twist.hpp"
+#include "rclcpp/logging.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/float32_multi_array.hpp"
 
+/*
+ * Converts individual wheel angular velocities into robot body velocity
+ * (vx, vy, vw) for a 4-wheel omnidirectional drive robot.
+ *
+ * Wheel convention used:
+ *   u1 = Front Left wheel
+ *   u2 = Front Right wheel
+ *   u3 = Rear Right wheel
+ *   u4 = Rear Left wheel
+ *
+ * Kinematic model (mecanum inverse kinematics):
+ *
+ *   vx = (r/4) * ( u1 + u2 + u3 + u4 )
+ *   vy = (r/4) * (-u1 + u2 - u3 + u4 )
+ *   vw = (r / (4*(L + W))) * (-u1 + u2 + u3 - u4)
+ *
+ * where:
+ *   r = wheel radius
+ *   L = half of wheel base (front-rear distance / 2)
+ *   W = half of track width (left-right distance / 2)
+ *
+ */
 class KinematicModel : public rclcpp::Node {
   using Float32MultiArray = std_msgs::msg::Float32MultiArray;
   using Twist = geometry_msgs::msg::Twist;
@@ -18,30 +41,45 @@ public:
         [this](Float32MultiArray::SharedPtr msg) { callback(msg); });
     pub_ = create_publisher<Twist>("/cmd_vel", 10);
 
-    h_wheel_base_ = 0.170 / 2.0;
-    h_track_width_ = 0.26969 / 2.0;
-    radius_ = 0.100 / 2.0;
+    // Robot geometric parameters (meters)
+    h_wheel_base_ = 0.170 / 2.0;    // L
+    h_track_width_ = 0.26969 / 2.0; // W
+    radius_ = 0.100 / 2.0;          // r
     RCLCPP_INFO(get_logger(), "Initialized node");
   }
 
 private:
   void callback(const Float32MultiArray::SharedPtr msg) {
-    auto u_1 = msg->data[0];
-    auto u_2 = msg->data[1];
-    auto u_3 = msg->data[2];
-    auto u_4 = msg->data[3];
+    if (msg->data.size() != 4) {
+      RCLCPP_WARN(get_logger(), "Expects 4 wheel speeds, received %zu",
+                  msg->data.size());
+      return;
+    }
 
-    float vx = radius_ / 4.0 * (u_1 + u_2 + u_3 + u_4);
-    float vy = radius_ / 4.0 * (-u_1 + u_2 - u_3 + u_4);
+    auto [vx, vy, vw] =
+        get_velocity(msg->data[0], msg->data[1], msg->data[2], msg->data[3]);
+    publish_velocity(vx, vy, vw);
+  }
+
+  std::tuple<float, float, float> get_velocity(float u1, float u2, float u3,
+                                               float u4) const {
+    // Convert wheel angular velocity (rad/s) to robot body velocity
+    // (m/s,m/s,rad/s)
+    float vx = radius_ / 4.0 * (u1 + u2 + u3 + u4);
+    float vy = radius_ / 4.0 * (-u1 + u2 - u3 + u4);
     float vw = radius_ / (4.0 * (h_wheel_base_ + h_track_width_)) *
-               (-u_1 + u_2 + u_3 - u_4);
+               (-u1 + u2 + u3 - u4);
+    return {vx, vy, vw};
+  }
 
+  void publish_velocity(float vx, float vy, float vw) const {
     Twist cmd_vel;
     cmd_vel.linear.x = vx;
     cmd_vel.linear.y = vy;
     cmd_vel.angular.z = vw;
     pub_->publish(cmd_vel);
   }
+
   rclcpp::Subscription<Float32MultiArray>::SharedPtr sub_;
   rclcpp::Publisher<Twist>::SharedPtr pub_;
   float h_wheel_base_;
